9bf2d0d8b40c2a66f1a346344862486ec17b9c83
Katto <kattogamet@gmail.com>
2025-02-21 20:19:53 -0300

    great commit

---
+++ src/data/networks.ts
@@ -0,0 +1,21 @@
+import Discord from "../components/Icons/Discord.astro"
+import Twitter from "../components/Icons/Twitter.astro"
+import Instagram from "../components/Icons/Instagram.astro"
+
+export const NETWORKS = [
+    {
+        label: 'Twitter page',
+        icon: Twitter,
+        href: '#'
+    },
+    {
+        label: 'Instagram page',
+        icon: Instagram,
+        href: '#'
+    },
+    {
+        label: 'Discord community',
+        icon: Discord,
+        href: '#'
+    },
+]
